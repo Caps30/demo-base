471153bc3819cbef91c04e7cf0a490b634d6a3c1
Caps <1921885889@qq.com>
2022-06-13 18:34:13 +0800

    git测试

---
+++ src/main/java/com/example/demo/ControllerA.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/yunxiao/test")
+public class ControllerA {
+    @GetMapping("/test1")
+    public String test1() {
+        return "test1接口";
+    }
+}
